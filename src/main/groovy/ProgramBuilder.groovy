cbfd02591db57db6b5f28f1eacdf67a742f9f035
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2011-08-31 21:07:27 +0200

    Default program test
    The default program is added to ensure the correct behaviour.

--- src/main/groovy/ProgramBuilder.groovy
+++ src/main/groovy/ProgramBuilder.groovy
@@ -1,3 +1,18 @@
-class ProgramBuilder {
+import program.DoProgram
+
+class ProgramBuilder extends BuilderSupport {
+	protected void setParent(Object parent, Object child) {}
+    
+    protected Object createNode(Object name){
+    	return new DoProgram()
+    }
+    
+    protected Object createNode(Object name, Object value){}
+       	
+    protected Object createNode(Object name, Map attributes){}
+    
+    protected Object createNode(Object name, Map attributes, Object value){}
+    
+    protected void nodeCompleted(Object parent, Object node) {}
 
 }
--- src/test/groovy/ProgramBuilderTest.groovy
+++ src/test/groovy/ProgramBuilderTest.groovy
@@ -1,13 +1,33 @@
 import groovy.util.GroovyTestCase
 
+import program.Program
+import robot.Robot
+import robot.LoggerRobot
+
 class ProgramBuilderTest extends GroovyTestCase {
+	private ProgramBuilder builder
+	private Robot robot
+	
+	public void setUp() {
+		builder = new ProgramBuilder()
+		robot = new LoggerRobot()
+	}
+	
 	public void testThatProgramBuilderExists() {
 		assert new ProgramBuilder() != null;
 	}
 	
 	public void testThatProgramBuilderCanBuildAProgram() {
-		Program program = new ProgramBuilder().program()
+		Program program = builder.do()
 		
 		assert program != null
+	}
+	
+	public void testThatAnEmptyProgramDoesNothingWithARobot() {
+		Program program = builder.do()
+		
+		program.executeWith(robot)
+		
+		assert "" == robot.toString()
 	}
 }
