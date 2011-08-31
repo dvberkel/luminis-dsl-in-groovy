15d695464799d71d2a829dfbeb1bc479096168d9
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2011-08-31 18:33:02 +0200

    Forward Program Test
    The forward program is tested to execute correctly.

--- src/test/groovy/ProgramBuilderTest.groovy
+++ src/test/groovy/ProgramBuilderTest.groovy
@@ -4,4 +4,10 @@ class ProgramBuilderTest extends GroovyTestCase {
 	public void testThatProgramBuilderExists() {
 		assert new ProgramBuilder() != null;
 	}
+	
+	public void testThatProgramBuilderCanBuildAProgram() {
+		Program program = new ProgramBuilder().program()
+		
+		assert program != null
+	}
 }
